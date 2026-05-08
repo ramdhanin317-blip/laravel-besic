09221f66627c753d7b1b408f74d416e9bf564307
Nia Ramadhani .R <ramdhanin317@gmail.com>
2026-05-08 23:15:26 +0800

    Pertemuan 5:Relationship

---
+++ database/seeders/LecturerSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Department;
+use App\Models\Lecturer;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class LecturerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Lecturer::factory()->count(500)->create();
+    }
+}
