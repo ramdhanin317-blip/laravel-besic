42c5796bd08419aeaf1e97d013fe624ae92d8b1f
Nia Ramadhani .R <ramdhanin317@gmail.com>
2026-05-09 14:18:14 +0800

    Pertemuan 6:Eager Loading, N+1 Problem

--- app/Http/Controllers/LecturerController.php
+++ app/Http/Controllers/LecturerController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Department;
 use App\Models\Lecturer;
 use Illuminate\Http\Request;
 
@@ -23,7 +24,10 @@ class LecturerController extends Controller
      */
     public function create()
     {
-        //
+        return view('lecturer.create', [
+            'title' => 'Create Lecturer',
+            'departments' => Department::all(),
+            ]);
     }
 
     /**
@@ -31,9 +35,22 @@ class LecturerController extends Controller
      */
     public function store(Request $request)
     {
-        //
-    }
+        {
+    $validated = $request->validate([
+        'name' => 'required|max:255',
+        'department_id' => 'required|exists:departments,id',
+    ], [
+        'name.required' => 'Nama tidak boleh kosong',
+        'name.max' => 'Nama tidak boleh lebih dari :max karakter',
+        'department_id.required' => 'Program Studi tidak boleh kosong',
+        'department_id.exists' => 'Program Studi yang dipilih tidak ditemukan',
+    ]);
 
+    Lecturer::create($validated);
+    return to_route('lecturer.index')->withSuccess('Data berhasil ditambahkan');
+        }
+    }
+    
     /**
      * Display the specified resource.
      */
@@ -47,7 +64,11 @@ class LecturerController extends Controller
      */
     public function edit(Lecturer $lecturer)
     {
-        //
+        return view('lecturer.edit', [
+            'title' => 'Edit Lecturer',
+            'lecturer' => $lecturer,
+            'departments' => Department::all()
+        ]);
     }
 
     /**
@@ -55,7 +76,18 @@ class LecturerController extends Controller
      */
     public function update(Request $request, Lecturer $lecturer)
     {
-        //
+        $validated = $request->validate([
+        'name' => 'required|max:255',
+        'department_id' => 'required|exists:departments,id',
+    ], [
+        'name.required' => 'Nama tidak boleh kosong',
+        'name.max' => 'Nama tidak boleh lebih dari :max karakter',
+        'department_id.required' => 'Program Studi tidak boleh kosong',
+        'department_id.exists' => 'Program Studi yang dipilih tidak ditemukan',
+    ]);
+
+    $lecturer->update($validated);
+    return to_route('lecturer.index')->withSuccess('Data berhasil diubah');
     }
 
     /**
@@ -63,6 +95,7 @@ class LecturerController extends Controller
      */
     public function destroy(Lecturer $lecturer)
     {
-        //
+    $lecturer->delete($lecturer);
+    return to_route('$lecturer.index')->withSuccess('Data berhasil dihapus');
     }
 }
--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -11,8 +11,8 @@
 
     <body>
         {{-- navigasi --}}
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
-            <div class="container-fluid">
+        <nav class="navbar navbar-expand-lg bg-warning navbar-dark">
+            <div class="container">
                 <a class="navbar-brand" href="#">UNITAMA</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                     data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
@@ -20,7 +20,7 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div class="navbar-nav">
+                    <div class="navbar-nav ms-auto">
                         <a class="nav-link active" href="{{ route('student.index') }}">Student</a>
                         <a class="nav-link active" href="{{ route('department.index') }}">Department</a>
                         <a class="nav-link active" href="{{ route('lecturer.index') }}">Lecturer</a>
